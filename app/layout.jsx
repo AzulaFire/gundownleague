5841a33b658d4ed0aeccd6064ee541775582ba04
AzulaFire <jhornjr@gmail.com>
2024-06-21 14:51:59 +0900

    fixed metadata client issue

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,25 @@
 import { Roboto } from 'next/font/google';
 import './globals.css';
 
+export const metadata = {
+  title: 'GunDown League - Fast-Paced Action & Sports Gameplay',
+  description:
+    'Experience the thrill of Ultimate Sports FPS, the fast-paced game that combines intense first-person shooter action with exciting sports gameplay. Join the action now!',
+  keywords: [
+    'GunDown League',
+    'GDL',
+    'FPS game',
+    'sports video game',
+    'first-person shooter',
+    'action sports game',
+    'multiplayer FPS',
+    'competitive gaming',
+    'eSports',
+    'online gaming',
+    'Ultimate Sports FPS',
+  ],
+};
+
 const roboto = Roboto({
   subsets: ['latin'],
   weight: ['100', '300', '400', '500', '700', '900'],
